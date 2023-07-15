41d688a166d61406a2a51a0ae3558f3df68d5a0c
Jihyeon Lee <strider13@snu.ac.kr>
2023-07-15 20:36:42 +0900

    Feat: 평면 구현

--- src/trace/hit/hit_plane.c
+++ src/trace/hit/hit_plane.c
@@ -6,7 +6,7 @@
 /*   By: jihyeole <jihyeole@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 16:04:10 by jihyeole          #+#    #+#             */
-/*   Updated: 2023/07/11 23:48:53 by jihyeole         ###   ########.fr       */
+/*   Updated: 2023/07/15 20:26:27 by jihyeole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@
 t_bool	hit_plane(t_object *pl_obj, t_ray *ray, t_hit_record *rec)
 {
 	t_plane	*pl;
+	double	root;
 
 	pl = pl_obj->element;
-	rec->t = (vdot(pl->point, pl->normal_vector) - \
+	root = (vdot(pl->point, pl->normal_vector) - \
 	vdot(ray->origin, pl->normal_vector)) \
 	/ vdot(ray->direction, pl->normal_vector);
-	if (rec->t <= 0)
+	if (root < rec->tmin || rec->tmax < root)
 		return (FALSE);
+	rec->t = root;
 	rec->p = ray_at(ray, rec->t);
 	rec->normal = pl->normal_vector;
 	set_face_normal(ray, rec);
